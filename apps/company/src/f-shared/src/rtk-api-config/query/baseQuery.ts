544f4a708c2dd91e598d638f979ddb03aaa421b9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-04-26 03:15:27 +0800

    Minor changes after merge

--- apps/company/src/f-shared/src/rtk-api-config/query/baseQuery.ts
+++ apps/company/src/f-shared/src/rtk-api-config/query/baseQuery.ts
@@ -6,7 +6,6 @@ export const baseQuery = fetchBaseQuery({
   credentials: 'include',
   prepareHeaders: (headers) => {
     // headers.set('Content-Type', 'application/json;charset=UTF-8');
-    headers.set('Authorization', 'Bearer eyJ0eXAiOiJBQ0NFU1MiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0ZXN0b3YyQG1haWwucnUiLCJ1c2VySWQiOjEsImlhdCI6MTcxMzg2Mzc2OCwiZXhwIjozNTEzODYzNzY4fQ.3wLkWt9xlmCliqM4t8oHzwE4X9P7O4pHUcpgPk0e18TXH3Uu7vQJY3WASLS0ZdQl')
     return headers;
   },
   responseHandler: async (response) => {
@@ -15,7 +14,6 @@ export const baseQuery = fetchBaseQuery({
       const token = tokenBearer?.replace('Bearer ', '');
       localStorage.setItem(Token_Enum.ACCESS_TOKEN, token);
     }
-
     try {
       return await response.json();
     } catch (error) {
--- apps/freelancer/src/f-shared/src/rtk-api-config/query/baseQuery.ts
+++ apps/freelancer/src/f-shared/src/rtk-api-config/query/baseQuery.ts
@@ -1,14 +1,11 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { Token_Enum } from '../enums/Token_Enum';
 
-
 export const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NODE_ENV === `development` ? `http://localhost:8080/` : process.env.NEXT_PUBLIC_URL ?? '',
   credentials: 'include',
   prepareHeaders: (headers) => {
-
     // headers.set('Content-Type', 'application/json;charset=UTF-8');
-    headers.set('Authorization', 'Bearer eyJ0eXAiOiJBQ0NFU1MiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0ZXN0b3YzQG1haWwucnUiLCJ1c2VySWQiOjIsImlhdCI6MTcxMzk1ODM1NSwiZXhwIjoxNzMxOTU4MzU1fQ.o_3CdJUfLnFiD-8sGKtqfFzbvV_BhbqRLYT8l0t1YRRF9PfUnZ3IcQCSPyDF3x6S')
     return headers;
   },
   responseHandler: async (response) => {
