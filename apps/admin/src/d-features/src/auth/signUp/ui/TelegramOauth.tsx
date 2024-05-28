8edc5e3b9f9e3306d74f8ba6f17483338f588022
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-05-28 12:46:33 +0800

    Tg bot vars

--- apps/admin/src/d-features/src/auth/signUp/ui/TelegramOauth.tsx
+++ apps/admin/src/d-features/src/auth/signUp/ui/TelegramOauth.tsx
@@ -17,7 +17,7 @@ export function TelegramOauth(props: Props) {
     const script = document.createElement('script');
 
     script.src = 'https://telegram.org/js/telegram-widget.js?21';
-    script.setAttribute('data-telegram-login', process.env.NEXT_PUBLIC_BOT_URL ?? 'FreelbeeAdminTest1Bot'); //TODO::: change bot
+    script.setAttribute('data-telegram-login', process.env.NEXT_PUBLIC_ADMIN_BOT_URL ?? 'FreelbeeAdminTest1Bot');
     script.setAttribute('data-size', `large`);
     script.setAttribute('data-request-access', `write`);
     script.setAttribute('data-userpic', `false`);
