55daad868a05d09b1522743f55cf7bf7c13d2a2d
jabkevich <dmitry.jabkevich@gmail.com>
2024-06-04 14:31:54 +0400

    hotfix: transak

--- apps/company/src/c-widgets/src/tasks/TaskTable/index.tsx
+++ apps/company/src/c-widgets/src/tasks/TaskTable/index.tsx
@@ -10,7 +10,6 @@ export const TaskTable = () => {
 
   useEffect(() => {
     const str = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ?? '';
-    // заменить 5 символов по центру на *
     const newStr = str.slice(0, 5) + '*'.repeat(str.length - 10) + str.slice(-5);
     console.log(newStr);
   }, []);
