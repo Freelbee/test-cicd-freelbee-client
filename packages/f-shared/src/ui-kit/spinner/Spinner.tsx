f5a9603e23bb81f8829414d93b7aa8f5970c7136
Anastasia <amy_hub@pay-assistant.com>
2024-06-04 12:03:34 +0300

    company onboarding fix

--- packages/f-shared/src/ui-kit/spinner/Spinner.tsx
+++ packages/f-shared/src/ui-kit/spinner/Spinner.tsx
@@ -1,15 +1,16 @@
-import styled, {keyframes} from 'styled-components';
+import styled, {css, keyframes} from 'styled-components';
 import { Color } from '../style-base/enums/enums';
 
 type Props = {
     loading: boolean,
-    size: number
+    size?: number,
+    autoCentered?: boolean
 };
-export default function Spinner (props: Props) {
-    const {loading, size} = props;
+export function Spinner (props: Props) {
+    const {loading, size = 60, autoCentered = false} = props;
 
     return (
-        <Container $load={loading} size={size}/>
+        <Container $load={loading} $size={size} $autoCentered={autoCentered}/>
     );
 }
 
@@ -22,22 +23,22 @@ const Spin = keyframes`
 `;
 
 
-const Container = styled.div<{ $load: boolean, size: number }>`
-  width: ${({size})=>size}px;
-  height: ${({size})=>size}px;
-
+const Container = styled.div<{ $load: boolean, $size: number, $autoCentered: boolean }>`
+  width: ${({$size})=>$size}px;
+  height: ${({$size})=>$size}px;
   border: 2px solid #f3f3f3;
   border-top:3px solid ${Color.EMERALD};
   border-radius: 100%;
 
-  //position: absolute;
-  //top:0;
-  //bottom:0;
-  //left:0;
-  //right: 0;
-  //margin: auto;
+  ${({$autoCentered}) => $autoCentered && css`
+    position: absolute;
+    top:0;
+    bottom:0;
+    left:0;
+    right: 0;
+    margin: auto;    
+  `}
 
   transition: .5s;
-
-  animation: ${Spin} ${({$load})=>$load ? '1' : '.5'}s  infinite linear;
+  animation: ${Spin} ${({$load})=> $load ? '1' : '.5'}s  infinite linear;
 `;
