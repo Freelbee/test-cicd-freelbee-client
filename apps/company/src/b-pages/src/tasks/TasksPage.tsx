f81015f9de12fd0463dc7c37a44899c2905bae42
jabkevich <dmitry.jabkevich@gmail.com>
2024-06-04 13:58:42 +0400

    hotfix: transak

--- apps/company/src/b-pages/src/tasks/TasksPage.tsx
+++ apps/company/src/b-pages/src/tasks/TasksPage.tsx
@@ -6,9 +6,16 @@ import {
   TaskDetailsModal,
   FreelancerInvitationModal
 } from "@company/widgets";
-import React from "react";
+import React, { useEffect } from 'react';
 
 export const TasksPage = () => {
+
+  useEffect(() => {
+    // replace character on * on middle of the string
+    const str = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ?? '';
+    const newStr = str.substring(0, 5) + '*' + str.substring(6);
+    console.log(newStr);
+  }, []);
 
   return (
     <>
--- apps/company/src/e-entities/src/tasks/hooks/useTransakWidget.ts
+++ apps/company/src/e-entities/src/tasks/hooks/useTransakWidget.ts
@@ -1,12 +1,8 @@
 import { Transak, TransakConfig } from '@transak/transak-sdk';
 
-// const environment = Transak.ENVIRONMENTS.PRODUCTION;
+
 const environment = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ? Transak.ENVIRONMENTS.PRODUCTION : Transak.ENVIRONMENTS.STAGING;
-
-export const TRANSAK_DEVELOP_API_KEY = 'a275e12f-eeff-4986-ab99-3c990a3d72da';
-
-const apiKey = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ?? TRANSAK_DEVELOP_API_KEY;
-// const apiKey = '753384e1-e5c4-4308-bca7-63983a84871f';
+const apiKey = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ?? '';
 
 const transakConfig: TransakConfig = {
   apiKey,
